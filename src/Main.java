3317be0e1509418297f039dae371680f3c0ea73c
HoxXxi <hoxxxii@gmail.com>
2017-11-19 00:38:39 +0000

    debugged

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 public class Main {
 
     static String reference = "ABCDEFGHIJKLMNOPQRSTUVWXYZ '";
-    static String line = "PAIN IS ONLY TEMPORARY BUT VICTORY IS FOREVER";
+    static String line = "WINNERS DON'T WAIT FOR CHANCES THEY TAKE THEM";
     public static void main(String[]args) {
         //TODO Remove from
         for(int i = 0; i < reference.length(); i ++) {
@@ -27,22 +27,24 @@ public class Main {
             System.out.print("\t"+distance(line.charAt(i),line.charAt(i+1)));
         }        System.out.println();System.out.println();
         //TODO Remove to
-        double time = 1;
+        double time = line.length();
         double distance = 0;
         for(int i = 0; i < line.length()-1; i ++) {
             distance += distance(line.charAt(i),line.charAt(i+1));
-            time += 1;
         }
+
+        System.out.println(distance);
+        distance = 324;
         double timeRunning = (distance*60.0*Math.PI/28.0)/15.0;
-        System.out.println(time+timeRunning);
+        System.out.println(time);
+        System.out.println(timeRunning);
     }
 
     private static int distance(char c, char c1) {
-        int antiClockWiseMoves=Math.abs(reference.indexOf(c)-reference.indexOf(c1))-1;
-        if(antiClockWiseMoves==-1)
-            return 0;
-        if(antiClockWiseMoves<reference.length()/2)
-            return antiClockWiseMoves;
-        return (reference.length()-2)-antiClockWiseMoves;
+        int antiClockWiseMoves=Math.abs(reference.indexOf(c)-reference.indexOf(c1));
+        if(antiClockWiseMoves>=reference.length()/2) {
+            return reference.length()-antiClockWiseMoves;
+        }
+        return antiClockWiseMoves;
     }
 }
